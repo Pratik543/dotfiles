541dc51903b027ae02702c7f5b8ee39c5a612e5a
Pratik <71585947+Pratik543@users.noreply.github.com>
2024-01-05 01:54:11 +0530

    chore(minor): added wrap for fzf preview

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -70,11 +70,11 @@ function longlisting{
 
 function previewFileWithSyntaxHighlighting {
     # This function uses fzf to select a file and bat to preview the contents of the selected file with syntax highlighting.
-    fzf --preview-window=60% --preview 'bat --style=numbers,grid --wrap=auto --color=always {}'
+    fzf --preview-window='60%,wrap' --preview 'bat {} --style=numbers --color=always'
 }
 
 function previewtldrpages {
-    tldr --list | fzf --preview "tldr {1} --color=always" --preview-window=right,70% 
+    tldr --list | fzf --preview "tldr {1} --color=always" --preview-window=right,70%,wrap 
 }
 
 function ExplorerFromHere { 
