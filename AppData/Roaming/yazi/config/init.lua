f943c40d329517d42f5000e9fea409d396273d99
Pratik <71585947+Pratik543@users.noreply.github.com>
2025-09-04 23:12:43 +0530

    fix(yazi): updated init file with comments

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -1,5 +1,13 @@
+-- [plugins] - git
 require("git"):setup()
+
+-- [plugins] - full-border
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 })
+
+-- [plugins] - zoxide (builtin)
+require("zoxide"):setup {
+	update_db = true, -- update db.zo automatically whenever move directories
+}
