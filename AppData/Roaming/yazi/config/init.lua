d2326f6175f73fb3f5c95053ee13d878354a7431
Pratik <71585947+Pratik543@users.noreply.github.com>
2026-03-24 18:48:26 +0530

    fix: update yazi config and added new plugins

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -11,3 +11,10 @@ require("full-border"):setup({
 require("zoxide"):setup {
 	update_db = true, -- update db.zo automatically whenever move directories
 }
+
+-- DuckDB plugin configuration
+require("duckdb"):setup({
+  mode = "standard",                          -- Default: "summarized"
+  cache_size = 1000,                          -- Default: 500
+  row_id = true,                         -- Default: false
+})
